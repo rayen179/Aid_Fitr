cc59c700cba1d54a48f912c8e88b52e7e3a9efbc
rayencherni589@gmail.com <rayencherni589@gmail.com>
2026-03-13 06:06:19 +0100

    Aid_Fitr

---
+++ js/script.js
@@ -0,0 +1,21 @@
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#144bb8",
+                "background-light": "#f6f6f8",
+                "background-dark": "#111621",
+            },
+            fontFamily: {
+                "display": ["Manrope", "sans-serif"]
+            },
+            borderRadius: {
+                "DEFAULT": "0.25rem",
+                "lg": "0.5rem",
+                "xl": "0.75rem",
+                "full": "9999px"
+            },
+        },
+    },
+}
